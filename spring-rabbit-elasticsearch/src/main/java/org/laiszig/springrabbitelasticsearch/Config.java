6a1a3a93e013640d03f2f11545492b95f9844dba
laiszig <laisziegler@gmail.com>
2025-02-09 23:50:42 -0300

    add config and rabbit consumer

---
+++ spring-rabbit-elasticsearch/src/main/java/org/laiszig/springrabbitelasticsearch/Config.java
@@ -0,0 +1,14 @@
+package org.laiszig.springrabbitelasticsearch;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
--- spring-rabbit-elasticsearch/src/main/java/org/laiszig/springrabbitelasticsearch/rabbit/RabbitConsumer.java
+++ spring-rabbit-elasticsearch/src/main/java/org/laiszig/springrabbitelasticsearch/rabbit/RabbitConsumer.java
@@ -1,15 +1,35 @@
 package org.laiszig.springrabbitelasticsearch.rabbit;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.IndexResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.laiszig.springrabbitelasticsearch.Document;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class RabbitConsumer {
 
+    private ElasticsearchClient esClient;
+    private ObjectMapper mapper;
+
+    @Autowired
+    public RabbitConsumer(ElasticsearchClient esClient, ObjectMapper mapper) {
+        this.esClient = esClient;
+        this.mapper = mapper;
+    }
+
     @RabbitListener(queues = "doc-queue")
-    public void receive(String content) {
+    public void receive(String content) throws IOException {
 
+        Document newDoc = mapper.readValue(content, Document.class);
+        esClient.index(i -> i
+                .index("documents")
+                .id(newDoc.id())
+                .document(newDoc));
         System.out.println(content);
-
     }
 }
